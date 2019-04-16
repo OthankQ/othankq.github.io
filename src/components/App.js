409ea1e642a9bc3a1c27fb7da3bc6d57636b9095
Ryan <kim1885@purdue.edu>
2019-04-16 13:36:19 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="colorlib-page">
+        <div className="container-wrap" />
+      </div>
+    );
+  }
+}
+
+export default App;
