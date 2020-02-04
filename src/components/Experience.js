26ade2818451ff5e30d003d1b84e96d54a5897b4
ryan <kim1885@purdue.edu>
2020-02-04 18:35:47 -0500

    Add experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,111 +1,164 @@
 import React from 'react';
 
 const Timeline = () => {
-  return (
-    <div>
-      <section className="colorlib-experience" data-section="experience">
-        <div className="colorlib-narrow-content">
-          <div className="row">
-            <div
-              className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-              data-animate-effect="fadeInLeft">
-              <span className="heading-meta">highlights</span>
-              <h2 className="colorlib-heading animate-box">Experience</h2>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-              <div className="timeline-centered">
-                <article
-                  className="timeline-entry animate-box"
-                  data-animate-effect="fadeInLeft">
-                  <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-3">
-                      <i className="icon-pen2" />
+    return (
+        <div>
+            <section className="colorlib-experience" data-section="experience">
+                <div className="colorlib-narrow-content">
+                    <div className="row">
+                        <div
+                            className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                            data-animate-effect="fadeInLeft">
+                            <span className="heading-meta">highlights</span>
+                            <h2 className="colorlib-heading animate-box">
+                                Experience
+                            </h2>
+                        </div>
                     </div>
-                    <div className="timeline-label">
-                      <h2>
-                        <a href="#">Teaching Assistant</a>
-                        <br />
-                        <span>Korean Naval Academy</span>
-                        <br />
-                        <span>2016-2018</span>
-                      </h2>
-                      <p>
-                        I Taught English to midshipmen at Korean Naval Academy
-                        during my service in the Navy. Reviewing and consulting
-                        midshipmen teaching material was also a part of my
-                        duties
-                      </p>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="timeline-centered">
+                                <article
+                                    className="timeline-entry animate-box"
+                                    data-animate-effect="fadeInLeft">
+                                    <div className="timeline-entry-inner">
+                                        <div className="timeline-icon color-2">
+                                            <i className="icon-pen2" />
+                                        </div>
+                                        <div className="timeline-label">
+                                            <h2>
+                                                <a href="#">
+                                                    Teaching Assistant
+                                                </a>
+                                                <br />
+                                                <span>
+                                                    Korean Naval Academy
+                                                </span>
+                                                <br />
+                                                <span>2016-2018</span>
+                                            </h2>
+                                            <p>
+                                                I Taught English to midshipmen
+                                                at Korean Naval Academy during
+                                                my service in the Navy.
+                                                Reviewing and consulting
+                                                midshipmen teaching material was
+                                                also a part of my duties
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article
+                                    className="timeline-entry animate-box"
+                                    data-animate-effect="fadeInLeft">
+                                    <div className="timeline-entry-inner">
+                                        <div className="timeline-icon color-3">
+                                            <i className="icon-pen2" />
+                                        </div>
+                                        <div className="timeline-label">
+                                            <h2>
+                                                <a href="#">
+                                                    Teaching Assistant
+                                                </a>
+                                                <br />
+                                                <span>
+                                                    Korean Naval Academy
+                                                </span>
+                                                <br />
+                                                <span>2016-2018</span>
+                                            </h2>
+                                            <p>
+                                                I Taught English to midshipmen
+                                                at Korean Naval Academy during
+                                                my service in the Navy.
+                                                Reviewing and consulting
+                                                midshipmen teaching material was
+                                                also a part of my duties
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article
+                                    className="timeline-entry animate-box"
+                                    data-animate-effect="fadeInTop">
+                                    <div className="timeline-entry-inner">
+                                        <div className="timeline-icon color-4">
+                                            <i className="icon-pen2" />
+                                        </div>
+                                        <div className="timeline-label">
+                                            <h2>
+                                                <a href="#">Interpreter</a>
+                                                <br />
+                                                <span>
+                                                    Korean Naval Academy
+                                                </span>
+                                                <br />
+                                                <span>2016-2018</span>
+                                            </h2>
+                                            <p>
+                                                Assisting in 2017 RIMPAC
+                                                exercise earned me a valuable
+                                                experience including working as
+                                                a team. Translating documents
+                                                and commands from English to
+                                                Korean and vice versa was one of
+                                                my main duties as an
+                                                interpreter. Korean Naval
+                                                Academy frequently hosts
+                                                visiting scholars and
+                                                dignitaries. Guiding them
+                                                through the base has been a
+                                                great chance to talk to people
+                                                around the world.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article
+                                    className="timeline-entry animate-box"
+                                    data-animate-effect="fadeInLeft">
+                                    <div className="timeline-entry-inner">
+                                        <div className="timeline-icon color-5">
+                                            <i className="icon-pen2" />
+                                        </div>
+                                        <div className="timeline-label">
+                                            <h2>
+                                                <a href="#">
+                                                    Web Content Manager
+                                                </a>
+                                                <br />
+                                                <span>Texas J&B</span>
+                                                <br />
+                                                <span>2011-2013</span>
+                                            </h2>
+                                            <p>
+                                                My main duty as a web content
+                                                manager was to periodically
+                                                upload and maintain the entire
+                                                product listing on the retail
+                                                website. This position taught me
+                                                a lot about what it means to
+                                                work on a website, which is most
+                                                of the time 'the face' of a
+                                                business.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article
+                                    className="timeline-entry begin animate-box"
+                                    data-animate-effect="fadeInBottom">
+                                    <div className="timeline-entry-inner">
+                                        <div className="timeline-icon color-none" />
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </article>
-                <article
-                  className="timeline-entry animate-box"
-                  data-animate-effect="fadeInTop">
-                  <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-4">
-                      <i className="icon-pen2" />
-                    </div>
-                    <div className="timeline-label">
-                      <h2>
-                        <a href="#">Interpreter</a>
-                        <br />
-                        <span>Korean Naval Academy</span>
-                        <br />
-                        <span>2016-2018</span>
-                      </h2>
-                      <p>
-                        Assisting in 2017 RIMPAC exercise earned me a valuable
-                        experience including working as a team. Translating
-                        documents and commands from English to Korean and vice
-                        versa was one of my main duties as an interpreter.
-                        Korean Naval Academy frequently hosts visiting scholars
-                        and dignitaries. Guiding them through the base has been
-                        a great chance to talk to people around the world.
-                      </p>
-                    </div>
-                  </div>
-                </article>
-                <article
-                  className="timeline-entry animate-box"
-                  data-animate-effect="fadeInLeft">
-                  <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-5">
-                      <i className="icon-pen2" />
-                    </div>
-                    <div className="timeline-label">
-                      <h2>
-                        <a href="#">Web Content Manager</a>
-                        <br />
-                        <span>Texas J&B</span>
-                        <br />
-                        <span>2011-2013</span>
-                      </h2>
-                      <p>
-                        My main duty as a web content manager was to
-                        periodically upload and maintain the entire product
-                        listing on the retail website. This position taught me a
-                        lot about what it means to work on a website, which is
-                        most of the time 'the face' of a business.
-                      </p>
-                    </div>
-                  </div>
-                </article>
-                <article
-                  className="timeline-entry begin animate-box"
-                  data-animate-effect="fadeInBottom">
-                  <div className="timeline-entry-inner">
-                    <div className="timeline-icon color-none" />
-                  </div>
-                </article>
-              </div>
-            </div>
-          </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default Timeline;
