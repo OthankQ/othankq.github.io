084e04278e3c5c4a467f1b7cab18e0ba8fc3923d
Ryan Kim <ryank9462@gmail.com>
2021-03-16 21:45:21 -0500

    Updates

---
+++ precache-manifest.2095a053646b3531f7df1a0d05413aa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aaa815c9afcdeb4ce88d",
+    "url": "/static/js/main.ceb8efec.chunk.js"
+  },
+  {
+    "revision": "990d05e0b097cbc57982",
+    "url": "/static/js/2.55e810db.chunk.js"
+  },
+  {
+    "revision": "aaa815c9afcdeb4ce88d",
+    "url": "/static/css/main.8a83c9e5.chunk.css"
+  },
+  {
+    "revision": "5daf5cadefc37c13d1632c1ddbdb8011",
+    "url": "/index.html"
+  }
+];
